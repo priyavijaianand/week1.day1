3830aead52e6b29f9a8be13c7d252fe370ae8e4f
priyavijaianand <91800956+priyavijaianand@users.noreply.github.com>
2021-10-19 02:27:48 +0530

    Add files via upload

---
+++ PositiveOrNegative.java
@@ -0,0 +1,22 @@
+package assignments;
+
+public class PositiveOrNegative 
+{
+public static void main(String[] args) 
+{
+	int i = 0;
+	
+	if (i < 0) 
+	{
+		System.out.println("no is -ve");
+	}
+	else if (i > 0) 
+	{
+		System.out.println("no is +ve");
+	}
+	else 
+	{
+		System.out.println("The no is neither +ve nor -ve");
+	}
+}
+}
